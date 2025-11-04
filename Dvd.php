02559706542dfa35a1fe8f4ba1d4675371544715
Fran Santos <francisco.sangue.1@educa.jcyl.es>
2025-11-04 20:45:03 +0100

    Todo hecho hasta antes de la clase Cliente

--- Dvd.php
+++ Dvd.php
@@ -2,5 +2,20 @@
 require_once("Soporte.php");
 
 class Dvd extends Soporte{
-    
+    protected $idiomas;
+    protected $formatPantalla;
+
+    //Llamo al constructor de la clase padre y añado los nuevos atributos
+    public function __construct($titulo, $numero, $precio, $idiomas, $formatPantalla)
+    {
+        parent::__construct($titulo, $numero, $precio);
+        $this->idiomas = $idiomas;
+        $this->formatPantalla = $formatPantalla;
+    }
+
+    //Añados los nuevos atributos al resumen
+    public function muestraResumen(){
+        parent::muestraResumen();
+        echo "Idiomas: " . $this->idiomas . "<br>";
+    }
 }
--- Juego.php
+++ Juego.php
@@ -1,6 +1,41 @@
 <?php
 require_once("Soporte.php");
 
-class Juego extends Soporte{
-    
+class Juego extends Soporte
+{
+    protected $consola;
+    protected $minNumJugadores;
+    protected $maxNumJugadores;
+
+    //Llamo al constructor de la clase padre y añado los nuevos atributos
+    public function __construct($titulo, $numero, $precio, $consola, $minNumJugadores, $maxNumJugadores)
+    {
+        parent::__construct($titulo, $numero, $precio);
+        $this->consola = $consola;
+        $this->minNumJugadores = $minNumJugadores;
+        $this->maxNumJugadores = $maxNumJugadores;
+    }
+
+    //Compruebo las posibles combinaciones de jugadores y devuelvo el texto adecuado
+    public function muestraJugadoresPosibles()
+    {
+        if ($this->minNumJugadores === $this->maxNumJugadores) {
+            if ($this->minNumJugadores === 1) return "Para un jugador";
+            return "Para {$this->minNumJugadores} jugadores";
+        }
+
+        if ($this->minNumJugadores === 1 && $this->maxNumJugadores > 1) {
+            return "De 1 a {$this->maxNumJugadores} jugadores";
+        }
+
+        return "De {$this->minNumJugadores} a {$this->maxNumJugadores} jugadores";
+    }
+
+    //Añados los nuevos atributos al resumen
+    public function muestraResumen()
+    {
+        parent::muestraResumen();
+        echo "Consola: " . $this->consola . " <br>";
+        echo $this->muestraJugadoresPosibles() . "<br>";
+    }
 }
