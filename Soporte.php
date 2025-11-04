81d227ae23abc9d25477a0d201805db89d61f7e3
Fran Santos <francisco.sangue.1@educa.jcyl.es>
2025-11-04 19:23:56 +0100

    Archivos php para las clases creadas

--- Soporte.php
+++ Soporte.php
@@ -1,1 +1,20 @@
 <?php
+
+class Soporte{
+    protected $titulo;
+    protected $numero;
+    protected $precio;
+
+    private const IVA = 0.21;
+
+    public function __construct($titulo, $numero, $precio)
+    
+    {
+        $this->titulo = $titulo;
+        $this->numero = $numero;
+        $this->precio = $precio;
+    }
+
+
+}
+
